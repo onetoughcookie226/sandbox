d88c405a32c0f0ef363dd9cf23ed0f8070097ff6
onetoughcookie226 <onetoughcookie226@gmail.com>
2017-02-14 18:32:45 +0100

    modifying elements using textContent

--- app.js
+++ app.js
@@ -3,5 +3,19 @@ const p = document.querySelector('p.description');
 const button= document.querySelector('button');
 
 button.addEventListener('click', () => {
-    p.textContent = input.value + ':';
+    p.innerHTML = input.value + ':';
 });
+
+p.title = "List description";
+
+
+/*above is the EMCA6 syntax for anonymous function*/
+// button.addEventListener('click', function() {
+//     p.innerHTML = input.value + ':';
+// });
+//
+//
+// function myFunction() {
+//     p.innerHTML = input.value + ':';
+// }
+// button.addEventListener('click', myFunction());

0d367bf3c9fdafd75cfe1843023415948041a598
onetoughcookie226 <onetoughcookie226@gmail.com>
2017-02-15 22:04:43 +0100

    use toggleList to show and hide content

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+const toggleList = document.getElementById('toggleList');
+
+const listDiv = document.querySelector('.list');
+
 const input = document.querySelector('input');
 const p = document.querySelector('p.description');
 const button= document.querySelector('button');
+
+
+toggleList.addEventListener('click', () => {
+    listDiv.style.display = 'none';
+
+});
 
 button.addEventListener('click', () => {
     p.innerHTML = input.value + ':';

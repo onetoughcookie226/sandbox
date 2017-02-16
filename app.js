43d6dbcdb27d8a4163c916aeca2e215076f0d9a6
onetoughcookie226 <onetoughcookie226@gmail.com>
2017-02-16 23:03:26 +0100

    Add another button and remove alst item in the list button

--- app.js
+++ app.js
@@ -35,7 +35,13 @@ addItemButton.addEventListener('click', () => {
     let li = document.createElement('li');
     li.textContent = addItemInput.value;
     ul.appendChild(li);
+    addItemInput.value= '';
+});
 
+removeItemButton.addEventListener('click', () => {
+    let ul = document.getElementsByTagName('ul')[0];
+    let li = document.querySelector('li:last-child');
+    ul.removeChild(li);
 });
 
 

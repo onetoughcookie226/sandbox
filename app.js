ff72079dce41cb948f0d66f84e1ec926ebe56015
onetoughcookie226 <onetoughcookie226@gmail.com>
2017-02-16 22:44:43 +0100

    fixed a typo

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const listDiv = document.querySelector('.list');
 const descriptionInput = document.querySelector('input.description');
 const descriptionP = document.querySelector('p.description');
 const descriptionButton= document.querySelector('button.description');
-const addItemInput = document.querySelector('input.addIteminput');
-const addItemutton = document.querySelector('button.addItemButton');
+const addItemInput = document.querySelector('input.addItemInput');
+const addItemButton = document.querySelector('button.addItemButton');
+const removeItemButton = document.querySelector('button.removeItemButton');
 
 toggleList.addEventListener('click', () => {
     if (listDiv.style.display == 'none') {
@@ -23,13 +24,17 @@ toggleList.addEventListener('click', () => {
 
 descriptionButton.addEventListener('click', () => {
     descriptionP.innerHTML = descriptionInput.value + ':';
+    descriptionP.title = "List description";
 });
 
-p.title = "List description";
 
-addItemutton.addEventListener('click', () => {
+
+addItemButton.addEventListener('click', () => {
+
+    let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li');
     li.textContent = addItemInput.value;
+    ul.appendChild(li);
 
 });
 
